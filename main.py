b4383b8143e0714e0c0fd07e2b2bec94e2e0b384
pouria-empire <arefijamal.pouria@gmail.com>
2023-06-29 18:42:21 +0330

    WRR complete

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import copy
 X = 5
 Y = 3
 T = 200
-SERVICE_POLICY = "NPPS"
+SERVICE_POLICY = "WRR"
 
 in_system_processes = dict()
 finished_processes = dict()
@@ -17,6 +17,10 @@ is_server_busy = False
 server_free_in = -1
 current_process_in_server = -1
 
+# from low to high
+WRR_Lines = [[]for _ in range(3)]
+WRR_Weights = [3,2,1] 
+Last_WRR_state = [3,3] #(priority, left)
 
 def generate_random_time(service_type):
     param = X if service_type == "interarrival" else Y
@@ -34,17 +38,23 @@ def generate_priority():
 
 
 def generate_process():
-    global next_process_in, process_number
     if next_process_in == time:
-        interarrival = generate_random_time("interarrival")
-        service_time = generate_random_time("service")
-        priority = generate_priority()
-        in_system_processes[process_number] = {"arrival_time": time, "service_time": service_time, "priority": priority}
-        print(f"process number {process_number} was added with arrival time = {time}, "
+        store_process()
+
+def store_process():
+    global next_process_in, process_number
+    interarrival = generate_random_time("interarrival")
+    service_time = generate_random_time("service")
+    priority = generate_priority()
+    in_system_processes[process_number] = {"arrival_time": time, "service_time": service_time, "priority": priority}
+        # add a process to its line
+    WRR_Lines[priority-1].append(process_number)
+
+    print(f"process number {process_number} was added with arrival time = {time}, "
               f"service time = {service_time} and priority = {priority}.")
-        print(f"next process will be in system {interarrival} seconds later.")
-        process_number += 1
-        next_process_in = time + interarrival
+    print(f"next process will be in system {interarrival} seconds later.")
+    process_number += 1
+    next_process_in = time + interarrival
 
 
 def get_next_process_for_npps():
@@ -77,38 +87,84 @@ def server():
         if SERVICE_POLICY == "FIFO":
             ids = in_system_processes.keys()
             current_id = min(ids)
-            in_system_processes[current_id]["in_server_time"] = time
-            current_process = in_system_processes[current_id]
-            current_process_in_server = current_id
-            service_time = current_process["service_time"]
-            is_server_busy = True
-            server_free_in = time + service_time
-            print(f"process number {current_id} got server in {time} and will hold it for {service_time} seconds")
+            select_process(current_id)
+
         elif SERVICE_POLICY == "WRR":
+            get_next_process_WRR()
+
+def get_next_process_WRR():
+    global Last_WRR_state
+    if Last_WRR_state[0] == 3 and Last_WRR_state[1] > 0:
+        if len(WRR_Lines[2]):
+            Last_WRR_state = [3,Last_WRR_state[1]-1]
+            current_id = WRR_Lines[2].pop(0)
+            select_process(current_id)
+            print(f"A process executed ftom line: 3")
+        elif len(WRR_Lines[1]):
+            Last_WRR_state = [2,1]
+            current_id = WRR_Lines[1].pop(0)
+            select_process(current_id) 
+            print(f"A process executed ftom line: 2")
+        elif len(WRR_Lines[0]):
+            Last_WRR_state = [1,0]
+            current_id = WRR_Lines[0].pop(0)
+            select_process(current_id) 
+            print(f"A process executed ftom line: 1")
+        else:
             pass
-        elif SERVICE_POLICY == "NPPS":
-            current_id = get_next_process_for_npps()
-            in_system_processes[current_id]["in_server_time"] = time
-            current_process = in_system_processes[current_id]
-            current_process_in_server = current_id
-            service_time = current_process["service_time"]
-            is_server_busy = True
-            server_free_in = time + service_time
-            print(f"process number {current_id} got server in {time} and will hold it for {service_time} seconds")
+    elif Last_WRR_state[0] == 2 and Last_WRR_state[1] > 0:
+        if len(WRR_Lines[1]):
+            Last_WRR_state = [2,Last_WRR_state[1]-1]
+            current_id = WRR_Lines[1].pop(0)
+            select_process(current_id)
+            print(f"A process executed ftom line: 2")
+        elif len(WRR_Lines[0]):
+            Last_WRR_state = [1,0]
+            current_id = WRR_Lines[0].pop(0)
+            select_process(current_id) 
+            print(f"A process executed ftom line: 1")
+        elif len(WRR_Lines[2]):
+            Last_WRR_state = [3,2]
+            current_id = WRR_Lines[2].pop(0)
+            select_process(current_id) 
+            print(f"A process executed ftom line: 3")
+        else:
+            pass
+    elif Last_WRR_state[0] == 1 and Last_WRR_state[1] > 0:
+        if len(WRR_Lines[0]):
+            Last_WRR_state = [1,Last_WRR_state[1]-1]
+            current_id = WRR_Lines[0].pop(0)
+            select_process(current_id)
+            print(f"A process executed ftom line: 1")
+        elif len(WRR_Lines[2]):
+            Last_WRR_state = [3,2]
+            current_id = WRR_Lines[2].pop(0)
+            select_process(current_id) 
+            print(f"A process executed ftom line: 3")
+        elif len(WRR_Lines[1]):
+            Last_WRR_state = [2,1]
+            current_id = WRR_Lines[1].pop(0)
+            select_process(current_id) 
+            print(f"A process executed ftom line: 2")
+        else:
+            pass
+
+
+def select_process(current_id):
+    global is_server_busy, server_free_in, current_process_in_server
+    in_system_processes[current_id]["in_server_time"] = time
+    current_process = in_system_processes[current_id]
+    current_process_in_server = current_id
+    service_time = current_process["service_time"]
+    is_server_busy = True
+    server_free_in = time + service_time
+    print(f"process number {current_id} got server in {time} and will hold it for {service_time} seconds")
 
 
 def start():
     global time, in_system_processes, process_number, next_process_in
     if time == 0:
-        interarrival = generate_random_time("interarrival")
-        service_time = generate_random_time("service")
-        priority = generate_priority()
-        in_system_processes[process_number] = {"arrival_time": time, "service_time": service_time, "priority": priority}
-        print(f"process number {process_number} was added with arrival time = {time}, "
-              f"service time = {service_time} and priority = {priority}.")
-        print(f"next process will be in system {interarrival} seconds later.")
-        process_number += 1
-        next_process_in = time + interarrival
+       store_process()
     while time <= T:
         generate_process()
         server()
